cded5a265d7ffb92e28951ec55d9de17be936590
hyeju <jm30806@naver.com>
2022-04-19 16:17:25 +0900

    1620번 나는야 포켓몬 마스터 이다솜 (해시)

---
+++ Baekjoon/1620.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+import java.util.HashMap;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        int n, m;
+        
+        // n (포켓몬 이름), m (맞춰야 하는 문제 수) 입력받기
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        
+        HashMap<String, String> map = new HashMap<>();
+        
+        // 포켓몬 이름 입력받기 (n번 반복)
+        for (int i = 1; i <= n; i++) {
+            String name = br.readLine();
+            String number = Integer.toString(i);
+            
+            map.put(name, number);
+            map.put(number, name);
+        }
+        
+        // 문제 맞추기 - 이름인 경우, key로 숫자인 경우, value로 찾기 (m번 반복)
+        for (int i = 0; i < m; i++) {
+            String quiz = br.readLine();
+            
+            System.out.println(map.get(quiz));
+        }
+    }
+}
